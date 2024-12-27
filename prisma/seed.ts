6eebdd8b87b74fe1352b7070a927ea279029a7b9
fabrichgit <fabrichjean@gmail.com>
2024-12-27 16:20:45 +0300

    chore: remake seed handler

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,18 @@
 import { db } from '../src/utils/db.server';
 
 async function seed() {
+    const user = await db.user.findUniqueOrThrow({
+        where: {
+            name: "fabrich"
+        }
+    })
+    if(user){
+        await db.user.delete({
+            where: {
+                id: user.id
+            }
+        })
+    }
     await db.user.create({
         data: {
             name: "fabrich",
